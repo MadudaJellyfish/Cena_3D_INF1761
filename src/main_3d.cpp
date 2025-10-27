b4bd7a130d419862fa7943e2474bd92a3db35fb1
maria eduarda <mariaeduardalivros@gmail.com>
2025-10-27 20:47:32 -0300

    Sistema solar em 3D com iluminação

--- src/main_3d.cpp
+++ src/main_3d.cpp
@@ -13,18 +13,86 @@
 #include "camera3d.h"
 #include "material.h"
 #include "transform.h"
-#include "cube.h"
 #include "sphere.h"
-#include "cylinder.h"
 #include "error.h"
 #include "shader.h"
 #include "light.h"
 #include "light.h"
-#include "polyoffset.h"
 #include "texture.h"
+#include "color.h"
+#include "quad.h"
+#include "engine.h"
 
 #include <iostream>
 #include <cassert>
+
+
+class MoveAstro;
+using MoveAstroPtr = std::shared_ptr<MoveAstro>;
+class MoveAstro : public Engine
+{
+    TransformPtr m_trf;
+    float m_angular_velocity;
+    float m_radius;
+    float m_angle;
+
+protected:
+    MoveAstro(TransformPtr trf, float velocity, float radius)
+        : m_trf(trf),
+        m_angular_velocity(velocity),
+        m_radius(radius),
+        m_angle(0.0f)
+    {
+    }
+public:
+    static MoveAstroPtr Make(TransformPtr trf, float velocity, float radius)
+    {
+        return MoveAstroPtr(new MoveAstro(trf, velocity, radius));
+    }
+    virtual void Update(float dt)
+    {
+        m_angle += -dt * m_angular_velocity;
+
+        m_trf->LoadIdentity();
+       // m_trf->Rotate(0, m_angle, 0, 1);
+        m_trf->Translate(m_radius*cos(m_angle), 0.0f, m_radius * sin(m_angle));
+    }
+
+};
+
+class AxisRotation;
+using AxisRotationPtr = std::shared_ptr<AxisRotation>;
+class AxisRotation : public Engine
+{
+    TransformPtr m_trf;
+    float m_angular_velocity;
+    float m_scale;
+    float m_angle;
+
+protected:
+    AxisRotation(TransformPtr trf, float velocity, float scale)
+        : m_trf(trf),
+        m_angular_velocity(velocity),
+        m_scale(scale),
+        m_angle(0.0f)
+    {
+    }
+public:
+    static AxisRotationPtr Make(TransformPtr trf, float velocity, float scale)
+    {
+        return AxisRotationPtr(new AxisRotation(trf, velocity, scale));
+    }
+    virtual void Update(float dt)
+    {
+        m_angle += -dt * m_angular_velocity;
+
+        m_trf->LoadIdentity();
+        m_trf->Scale(m_scale, m_scale, m_scale);
+        m_trf->Rotate(m_angle,0, 1, 0);
+    }
+
+};
+
 
 static float viewer_pos[3] = {2.0f, 3.5f, 4.0f};
 
@@ -42,66 +110,71 @@ static void initialize (void)
 
   // create objects
   camera = Camera3D::Make(viewer_pos[0],viewer_pos[1],viewer_pos[2]);
-  //camera->SetOrtho(true);
+
+  /// fim do nó de fundo
   arcball = camera->CreateArcball();
 
   //LightPtr light = ObjLight::Make(viewer_pos[0],viewer_pos[1],viewer_pos[2]);
-  LightPtr light = Light::Make(0.0f,0.0f,0.0f,1.0f,"camera");
-  light->SetPosition(5, 3, 0);
+  LightPtr light = Light::Make(0.0f,0.0f,0.0f,1.0f,"world");
+  light->SetPosition(0.0f, 0.0f, 0.0f);
   light->SetDiffuse(1.0f, 1.0f, 1.0f);
-  light->SetAmbient(0.5f, 0.5f, 0.5f);
+  light->SetAmbient(0.3f, 0.3f, 0.3f);
+  light->SetSpecular(0.0f, 0.0f, 0.0f);
 
+  AppearancePtr sun_material = Material::Make(1.0f, 0.0f, 0.0f);
   AppearancePtr white = Material::Make(1.0f, 1.0f, 1.0f);
   AppearancePtr light_greenyellow = Material::Make(0.62f,0.76f,0.19f);
   AppearancePtr ocean_green = Material::Make(0.0f,0.45f,0.45f);
   AppearancePtr beutifull_green = Material::Make(0.0f, 0.75f, 0.68f);
   AppearancePtr light_green = Material::Make(0.5f, 0.0f, 0.0f);
 
+  AppearancePtr sun = Texture::Make("decal", "images/sun.jpg");
   AppearancePtr earth = Texture::Make("decal", "images/earth.jpg");
   AppearancePtr mercury = Texture::Make("decal", "images/mercury.jpg");
+  AppearancePtr moon = Texture::Make("decal", "images/moon.jpeg");
   
-  //cilindro
-  TransformPtr trf_cilindro = Transform::Make();
-  trf_cilindro->Scale(0.3f, 1.0f, 0.3f);
-  trf_cilindro->Translate(-2.0f, 0.0f, -2.0f);
 
-  //mesa
-  TransformPtr trf1 = Transform::Make();
-  trf1->Scale(3.0f,0.3f,3.0f);
-  trf1->Translate(0.0f,-1.0f,0.0f);
+  //transf. sol
+  TransformPtr trf_sol = Transform::Make();
+  trf_sol->Scale(0.5f, 0.5f, 0.5f);
 
-  //bloquinho
-  TransformPtr trf_bloquinho = Transform::Make();
-  trf_bloquinho->Scale(1.0f, 0.25f, 1.0f);
-  trf_bloquinho->Translate(-0.5f, 0.0f, 0.5f);
+  //transf. mercúrio
+  TransformPtr trf_orbita_mercurio = Transform::Make();
+  TransformPtr trf_mercurio = Transform::Make();
+  trf_mercurio->Scale(0.15f, 0.15f, 0.15f);
+  //trf_mercurio->Translate(10.0f, 1.0f, 1.5f);
 
 
+  //transf. lua
+  TransformPtr trf_lua = Transform::Make();
+  trf_lua->Scale(0.2f, 0.2f, 0.2f);
+  //trf_lua->Translate(18.0f, 1.0f, 1.5f);
 
-  //esfera em cima do bloquinho
-  TransformPtr trf_esfera_2 = Transform::Make();
-  trf_esfera_2->Scale(0.2f, 0.2f, 0.2f);
-  trf_esfera_2->Translate(-2.0f, 2.3f, 2.0f);
+  TransformPtr trf_orbita_lua = Transform::Make();
 
 
-  //esfera em cima da mesa
-  TransformPtr trf2 = Transform::Make();
-  trf2->Scale(0.45f,0.45f,0.45f);
-  trf2->Translate(1.5f,1.0f,1.5f);
+  //transf. terra
+  TransformPtr trf_terra = Transform::Make();
+  //trf_terra->Scale(0.45f,0.45f,0.45f);
+  //trf_terra->Translate(10.0f,1.0f,1.5f);
 
+  TransformPtr trf_orbita_terra = Transform::Make();
+  TransformPtr trf_rotacao_terra = Transform::Make();
+
+  //contrução das geometrias
   Error::Check("before shps");
-  ShapePtr cube = Cube::Make();
   Error::Check("before sphere");
-  ShapePtr sphere = Sphere::Make();
-  Error::Check("before bloquinho");
-  ShapePtr bloquinho = Cube::Make();
-  Error::Check("before esfera bloquinho");
-  ShapePtr esfera_bloquinho = Sphere::Make();
-  Error::Check("before cilindro");
-  ShapePtr cilindro = Cylinder::Make();
+  ShapePtr sol = Sphere::Make();
+  Error::Check("before terra");
+  ShapePtr terra = Sphere::Make();
+  Error::Check("before lua");
+  ShapePtr lua = Sphere::Make();
+  Error::Check("before mercúrio");
+  ShapePtr mercurio = Sphere::Make();
   Error::Check("after shps");
+  //
 
-
-  // create shader
+    // create shader
   ShaderPtr shader = Shader::Make(light,"world");
   shader->AttachVertexShader("shaders/ilum_vert/vertex.glsl");
   shader->AttachFragmentShader("shaders/ilum_vert/fragment.glsl");
@@ -115,17 +188,33 @@ static void initialize (void)
   shd_tex->Link();
 
   // build scene
-  NodePtr esfera2 = Node::Make(trf_esfera_2, { light_green }, { esfera_bloquinho });
+
+  ///criacao do nós da cena
+
+  auto lua_node = Node::Make(shd_tex,trf_lua, { white, moon }, { lua });
+  auto lua_orbita_node = Node::Make(trf_orbita_lua, {}, {}, { lua_node });
+
+  auto terra_node = Node::Make(shd_tex,trf_terra, { white, earth }, { terra }, {});
+  auto terra_rotacao_node = Node::Make(trf_rotacao_terra, {}, {}, { terra_node, lua_orbita_node });
+  auto terra_orbita_node = Node::Make(trf_orbita_terra, {}, {}, { terra_rotacao_node });
+
+  auto mercurio_node = Node::Make(shd_tex,trf_mercurio, { white, mercury }, { mercurio }, {});
+  auto mercurio_orbita_node = Node::Make(trf_orbita_mercurio, {}, {}, { mercurio_node });
+
+  auto sol_node = Node::Make(shd_tex,trf_sol, { sun_material, sun }, { sol }, { mercurio_orbita_node, terra_orbita_node });
+
+  ///
+
+
 
   NodePtr root = Node::Make(shader,
-    {Node::Make(trf1,{ocean_green},{cube}),
-      Node::Make(trf_bloquinho, {beutifull_green}, {bloquinho}),
-     Node::Make(trf_esfera_2,{light_green},{esfera_bloquinho}),
-     Node::Make(shd_tex, trf2,{white, earth},{sphere}),
-     Node::Make(shd_tex, trf_cilindro,{white, mercury},{cilindro})
-    }
+    {sol_node}
   );
   scene = Scene::Make(root);
+  scene->AddEngine(MoveAstro::Make(trf_orbita_terra, 1.0f, 3.0f)); // Raio maior para a Terra
+  scene->AddEngine(MoveAstro::Make(trf_orbita_lua, 3.0f, 1.5f)); // Raio menor para a Lua (relativo à Terra)
+  scene->AddEngine(MoveAstro::Make(trf_orbita_mercurio, 1.5f, 1.5f)); // Raio menor para Mercúrio
+  scene->AddEngine(AxisRotation::Make(trf_rotacao_terra, 20.f, 0.45f)); // Velocidade de rotação menor
 }
 
 static void display (GLFWwindow* win)
@@ -183,6 +272,10 @@ static void mousebutton (GLFWwindow* win, int button, int action, int mods)
   else // GLFW_RELEASE 
     glfwSetCursorPosCallback(win, nullptr);      // callback disabled
 }
+static void update(float dt)
+{
+    scene->Update(dt);
+}
 
 int main ()
 {
@@ -214,7 +307,11 @@ int main ()
 
   initialize();
 
+  float t0 = float(glfwGetTime());
   while(!glfwWindowShouldClose(win)) {
+    float t = float(glfwGetTime());
+    update(t - t0);
+    t0 = t;
     display(win);
     glfwSwapBuffers(win);
     glfwPollEvents();
